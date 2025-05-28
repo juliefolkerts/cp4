d800fe4b7c01529ca197c722fdccf10aa775527d
Julie Folkerts <JulieVMF@gmail.com>
2025-05-28 13:24:29 +0100

    push

--- src/main/java/pl/folkerts/ecommerce/App.java
+++ src/main/java/pl/folkerts/ecommerce/App.java
@@ -2,11 +2,21 @@ package pl.folkerts.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.folkerts.productcatalog.ArrayListProductStorage;
+import pl.folkerts.productcatalog.ProductCatalog;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         System.out.println("It works");
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ProductCatalog createProductCatalog() {
+        return new ProductCatalog(
+                new ArrayListProductStorage()
+        );
     }
 }
